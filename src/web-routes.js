4fc0fab8eb8ac85fc56f5e36d44740e053f13697
Sheila Kirwan <sheilafleming81@gmail.com>
2022-03-21 22:09:04 +0000

    Admin account that lists and deletes users

--- src/web-routes.js
+++ src/web-routes.js
@@ -2,6 +2,7 @@ import { accountsController } from "./controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { traillistController } from "./controllers/traillist-controller.js";
+import { adminController } from "./controllers/admin-controller.js"
 
 export const webRoutes = [
   { method: "GET", path: "/", config: accountsController.index },
@@ -18,12 +19,12 @@ export const webRoutes = [
   { method: "GET", path: "/about", config: aboutController.index },
 
   { method: "GET", path: "/traillist/{id}", config: traillistController.index },
-  // { method: "POST", path: "/traillist/{id}/addtrail", config: traillistController.addTrail },
   { method: "POST", path: "/traillist/{id}/addtrail", config: traillistController.addTrail },
   { method: "GET", path: "/traillist/{id}/deletetrail/{trailid}", config: traillistController.deleteTrail },
   
 
-  { method: "GET", path: "/{param*}", handler: { directory: { path: "./public" } }, options: { auth: false } }
+  { method: "GET", path: "/{param*}", handler: { directory: { path: "./public" } }, options: { auth: false } },
 
-  
+  { method: "GET", path: "/admin", config: adminController.index },
+  { method: "GET", path: "/admin/deleteuser/{id}", config: adminController.deleteUser },
 ];
